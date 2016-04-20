cfed767b017388416957363c637353ca819afb87
unknown <453594574@qq.com>
2016-04-20 14:23:52 +0800

    add demo code

---
+++ js/require-config.js
@@ -0,0 +1,13 @@
+var config = {
+    baseUrl: "js/",           //依赖相对路径
+    paths: {                    //如果某个前缀的依赖不是按照baseUrl拼接这么简单，就需要在这里指出
+        zepto: 'zepto.min'
+    },
+    shim: {                     //引入没有使用requirejs模块写法的类库。backbone依赖underscore
+        'zepto': {
+            exports: '$'
+        }
+    }
+};
+
+require.config(config);
